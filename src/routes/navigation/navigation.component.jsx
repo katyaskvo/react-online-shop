fca5938e90e675e806d2f9ffb6cd5821d87d910a
Katya Zdorov <katyaskvo@gmail.com>
2022-05-25 18:36:10 -0500

    Add cart item and cart context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,10 +5,13 @@ import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import { CartContext } from '../../contexts/cart.context';
 
 const Navigation = () => {
 	const { loggedInUser } = useContext(UserContext);
-	
+	const { isCartOpen } = useContext(CartContext);
 	return (
 		<Fragment>
 			<div className="navigation">
@@ -23,8 +26,10 @@ const Navigation = () => {
 						loggedInUser ? <span className='nav-link' onClick={signOutUser}>Sign Out</span> :
 						<Link className="nav-link" to="/authentication">Sign In</Link>
 					}
-					
+					<CartIcon />
+		
 				</div>
+				{isCartOpen && <CartDropdown />}
 			</div>
 			<Outlet />
 		</Fragment>
