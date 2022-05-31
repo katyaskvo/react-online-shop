c80f0f5dc06779e82f472f1663e2e02088ad497d
Katya Zdorov <katyaskvo@gmail.com>
2022-05-31 17:03:08 -0500

    update button component to use styled components

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,19 +1,25 @@
-import './button.styles.scss';
+import { BaseButton, BlueButton, InvertedButton } from './button.styles.jsx';
 
 export const BUTTON_TYPE_CLASSES = {
+    base: 'base',
     blue: 'blue',
     inverted: 'inverted'
 };
 
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => 
+    ({
+        [BUTTON_TYPE_CLASSES.base]: BaseButton,
+        [BUTTON_TYPE_CLASSES.blue]: BlueButton,
+        [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+    }[buttonType]);
+
+
 const Button = ({children, buttonType, ...properties}) => {
-    
+    const CustomButton = getButton(buttonType);
     return (
-        <button 
-            className={`button-container ${buttonType}`}
-            {...properties}
-        >
+        <CustomButton {...properties}>
             {children}
-        </button>
+        </CustomButton>
     )
 }
 
